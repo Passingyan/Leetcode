bef728359fcbc7c36326e2043c61d3e72ce13185
yongzhenhuang <yong_zhenhuang@163.com>
2019-04-08 23:37:21 +0800

    两个有序数组求均值

--- src/main/java/yzh/Main.java
+++ src/main/java/yzh/Main.java
@@ -1,10 +1,22 @@
 package yzh;
 
 import yzh.algorithm.LongestSubstring;
+import yzh.algorithm.MedianSortedArrays;
 
 public class Main {
     public static void main(String[] args) {
+        findMedianSortedArrays();
+    }
+
+    public static void lengthOfLongestSubstring() {
         String s = "abcabcbb";
         System.out.println(LongestSubstring.lengthOfLongestSubstring2(s));
     }
+
+    public static void findMedianSortedArrays() {
+        int[] nums1 = {3};
+        int[] nums2 = {-2, -1};
+        System.out.println(MedianSortedArrays.findMedianSortedArrays(nums1, nums2));
+    }
+
 }
---
+++ src/main/java/yzh/algorithm/MedianSortedArrays.java
@@ -0,0 +1,45 @@
+package yzh.algorithm;
+
+/**
+ * @ClassName MedianSortedArrays
+ * @Author 78511
+ * @Date 2019/4/8 22:16
+ **/
+public class MedianSortedArrays {
+    /**
+     * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+     * 请你找出这两个有序数组的平均值，并且要求算法的时间复杂度为 O(log(m + n))。
+     */
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int medium = (nums1.length + nums2.length - 1) / 2;
+        double sum = MedianSortedArrays.calculateSumOfArrays(medium,
+                0, nums1.length + nums2.length - 1, nums1, nums2);
+        return sum / (nums1.length + nums2.length);
+    }
+
+    public static double calculateSumOfArrays(int medium, int lengthBeg, int lengthEnd, int[] nums1, int[] nums2) {
+        if (medium == lengthBeg) {
+            if (lengthBeg == lengthEnd) {
+                if (medium >= nums1.length) {
+                    return nums2[medium - nums1.length];
+                } else {
+                    return nums1[medium];
+                }
+            } else {
+                if (medium >= nums1.length) {
+                    return nums2[medium - nums1.length] + nums2[medium - nums1.length + 1];
+                } else {
+                    if (lengthEnd >= nums1.length) {
+                        return nums1[medium] + nums2[lengthEnd - nums1.length];
+                    } else {
+                        return nums1[medium] + nums1[lengthEnd];
+                    }
+                }
+            }
+        }
+        int leftMedium = (lengthBeg + medium) / 2;
+        int rightMedium = (lengthEnd + medium + 1) / 2;
+        return MedianSortedArrays.calculateSumOfArrays(leftMedium, lengthBeg, medium, nums1, nums2) +
+                MedianSortedArrays.calculateSumOfArrays(rightMedium, medium + 1, lengthEnd, nums1, nums2);
+    }
+}
