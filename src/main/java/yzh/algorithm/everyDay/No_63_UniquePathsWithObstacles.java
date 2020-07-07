2866d1a094394ab99a669cdb10ebb794f31b0766
yongzhenhuang <yongzhenhuang@jd.com>
2020-07-07 10:41:47 +0800

    one question per day

---
+++ src/main/java/yzh/algorithm/everyDay/No_63_UniquePathsWithObstacles.java
@@ -0,0 +1,65 @@
+package yzh.algorithm.everyDay;
+
+/**
+ * 63. 不同路径 II
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * <p>
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * <p>
+ * 说明：m 和 n 的值均不超过 100。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * [
+ *   [0,0,0],
+ *   [0,1,0],
+ *   [0,0,0]
+ * ]
+ * 输出: 2
+ * 解释:
+ * 3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-paths-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class No_63_UniquePathsWithObstacles {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null) return 0;
+        int row = obstacleGrid.length;
+        if (row == 0) return 0;
+        int col = obstacleGrid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                } else {
+                    if (i == 0 && j == 0) {
+                        obstacleGrid[i][j] = 1;
+                        continue;
+                    }
+                    int above = i == 0 ? 0 : obstacleGrid[i - 1][j];
+                    int left = j == 0 ? 0 : obstacleGrid[i][j - 1];
+                    obstacleGrid[i][j] = above + left;
+                }
+            }
+        }
+        return obstacleGrid[row - 1][col - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println(new No_63_UniquePathsWithObstacles().uniquePathsWithObstacles(arr));
+
+
+    }
+}
